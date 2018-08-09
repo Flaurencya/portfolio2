d4ddd9ff553f67e32eb02a0801286181ebf1c14f
Flaurencya <41689113+Flaurencya@users.noreply.github.com>
2018-08-09 15:05:27 -0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,11 +1,5 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
-
   };
-
   document.addEventListener('DOMContentLoaded', function () {
     var links = document.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++) {
